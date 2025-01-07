2d74c497509b95648902c395161a2c8df1282be4
poiu <ponpottboomk@gmail.com>
2025-01-07 14:02:17 +0700

    Create backend NodeJs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,40 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectToDatabase from "./config/db";
+import errorHandler from "./middleware/errorHandler";
+import authenticate from "./middleware/authenticate";
+import authRoutes from "./routes/auth.route";
+import userRoutes from "./routes/user.route";
+import sessionRoutes from "./routes/session.route";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
 
-const PORT = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+// add middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: APP_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.get("/", ( _, res) => {
+  res.status(200).json({ message: "Hello World" });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.use("/auth", authRoutes);
+app.use("/user", authenticate, userRoutes);
+app.use("/sessions", authenticate, sessionRoutes);
+
+// error handling middleware should be the last middleware
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+  console.log(`Server listening on port ${PORT} in ${NODE_ENV} environment`);
+  await connectToDatabase();
 });
